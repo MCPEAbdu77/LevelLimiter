879f78358e54e4039808990c1cfcda4f9bf82566
MCA7 <abdu77mcpe@gmail.com>
2021-04-09 21:38:44 +0200

    Add files via upload

---
+++ src/MCA7/LevelLimiter/Main.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MCA7\LevelLimiter;
+// By MCA7#1245
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat as C;
+use pocketmine\{event\server\CommandEvent, Player};
+use pocketmine\event\Listener;
+
+class Main extends PluginBase implements Listener {
+
+  public function onEnable() {
+
+    @mkdir($this->getDataFolder());
+    $this->saveDefaultConfig();
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+  public function onCMD(CommandEvent $event) {
+    $sender = $event->getSender();
+    $cmd = $event->getCommand();
+  	if($sender instanceof Player) {
+  		$con = $this->getConfig()->getAll();
+
+  		if (isset($con[$cmd])) {
+  			if (!in_array($sender->getLevel()->getName(), $this->getConfig()->get($cmd))) {
+  				$sender->sendMessage(C::DARK_RED. C::BOLD . "LevelLimiter :" .C::RED . " This command is disabled on this world!");
+  				$event->setCancelled();
+  				return true;
+  			}
+  		}
+  	}
+
+  }
+}
