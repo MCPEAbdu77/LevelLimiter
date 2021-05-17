e5b17cfa41b88eaf2f07382fbdac1607dbcec6dc
ErikPDev <33035080+ErikPDev@users.noreply.github.com>
2021-05-17 11:03:18 +0300

    Fixed bugs

--- src/MCA7/LevelLimiter/Main.php
+++ src/MCA7/LevelLimiter/Main.php
@@ -14,23 +14,23 @@ use pocketmine\event\Listener;
 class Main extends PluginBase implements Listener {
 
   public function onEnable() {
-
     @mkdir($this->getDataFolder());
     $this->saveDefaultConfig();
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
   }
+
   public function onCMD(PlayerCommandPreprocessEvent $event) {
     $sender = $event->getPlayer();
     $cmd = $event->getMessage();
-  	if($sender instanceof Player) {
+  	if(get_class($sender) == "pocketmine\Player") {
 		if($cmd[0] == "/") {
 			if (!$sender->hasPermission("levellimiter.bypass")) {
 				if (!$sender->hasPermission("levellimiter.bypass." . $sender->getLevel()->getName())) {
 					$cmdo = trim($cmd, "/");
-					if (!$sender->hasPermission("levellimiter.bypass." . $sender->getLevel()->getName() . "." . $cmdo)) {
+					if (!$sender->hasPermission("levellimiter.bypass." . $sender->getLevel()->getName() . "." . explode(" ",$cmdo)[0])) {
 						$con = $this->getConfig()->getAll();
-						if (isset($con[$cmdo])) {
-							if (!in_array($sender->getLevel()->getName(), $this->getConfig()->get($cmdo))) {
+						if (isset($con[explode(" ",$cmdo)[0]])) {
+							if (!in_array($sender->getLevel()->getName(), $this->getConfig()->get( explode(" ",$cmdo)[0] ))) {
 								$sender->sendMessage(C::DARK_RED . C::BOLD . "LevelLimiter :" . C::RESET . C::RED . " This command is disabled on this world!");
 								$event->setCancelled();
 								return true;
