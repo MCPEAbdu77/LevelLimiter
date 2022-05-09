7f51106a3157e8f3cc97033b1f34668e410e829c
MCA7 <abdu77mcpe@gmail.com>
2022-05-09 22:46:01 +0200

    Update Main.php

--- src/MCA7/LevelLimiter/Main.php
+++ src/MCA7/LevelLimiter/Main.php
@@ -8,35 +8,35 @@ namespace MCA7\LevelLimiter;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat as C;
-use pocketmine\{event\player\PlayerCommandPreprocessEvent, Player};
+use pocketmine\{event\player\PlayerCommandPreprocessEvent, event\Player};
 use pocketmine\event\Listener;
 
 class Main extends PluginBase implements Listener {
 
-  public function onEnable() {
+  public function onEnable():void {
     @mkdir($this->getDataFolder());
     $this->saveDefaultConfig();
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
   }
 	
 
-  public function onCMD(PlayerCommandPreprocessEvent $event) {
+  public function onCMD(PlayerCommandPreprocessEvent $event):void {
     $sender = $event->getPlayer();
     $cmd = $event->getMessage();
     $prefix = $this->getConfig()->get("prefix");
     $msg = $this->getConfig()->get("blocked-message");
-  	if(get_class($sender) == "pocketmine\Player") {
+  	if(get_class($sender) == "pocketmine\player\Player") {
 		if($cmd[0] == "/") {
 			if (!$sender->hasPermission("levellimiter.bypass")) {
-				if (!$sender->hasPermission("levellimiter.bypass." . $sender->getLevel()->getName())) {
+				if (!$sender->hasPermission("levellimiter.bypass." . $sender->getWorld()->getFolderName())) {
 					$cmdo = trim($cmd, "/");
-					if (!$sender->hasPermission("levellimiter.bypass." . $sender->getLevel()->getName() . "." . explode(" ",$cmdo)[0])) {
+					if (!$sender->hasPermission("levellimiter.bypass." . $sender->getWorld()->getFolderName() . "." . explode(" ",$cmdo)[0])) {
 						$con = $this->getConfig()->getAll();
 						if (isset($con[explode(" ",$cmdo)[0]])) {
-							if (!in_array($sender->getLevel()->getName(), $this->getConfig()->get( explode(" ",$cmdo)[0] ))) {
+							if (!in_array($sender->getWorld()->getFolderName(), $this->getConfig()->get( explode(" ",$cmdo)[0] ))) {
 								$sender->sendMessage($prefix . C::RESET . " " . $msg);
-								$event->setCancelled();
-								return true;
+								$event->isCancelled(true);
+								return;
 							}
 						}
 					}
