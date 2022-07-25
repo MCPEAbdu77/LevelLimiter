eba43a91ca4c401dbd205b3245f174f88549f038
MCA7 <abdu77mcpe@gmail.com>
2022-07-25 19:42:58 +0300

    Added blacklist mode

--- src/MCA7/LevelLimiter/Main.php
+++ src/MCA7/LevelLimiter/Main.php
@@ -3,45 +3,68 @@
 declare(strict_types=1);
 
 namespace MCA7\LevelLimiter;
-// By MCA7#1245
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat as C;
-use pocketmine\{event\player\PlayerCommandPreprocessEvent, event\Player};
+use pocketmine\{event\Player, 
+	event\player\PlayerCommandPreprocessEvent};
 use pocketmine\event\Listener;
 
-class Main extends PluginBase implements Listener {
 
-  public function onEnable():void {
+class Main extends PluginBase implements Listener 
+{
 
-    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+  public function onEnable() : void 
+  {
+
+    	$this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+  }
+
+
+  public function onLoad() : void 
+  {
+		if(!($this->getConfig()->get('ver')) || $this->getConfig()->get('ver') !== '1.0') {
+			$this->getServer()->getLogger()->debug(
+				C::RED . 'Invalid Config version - Update plugin or delete the old config file! Disabling plugin.'
+			);
+			$this->getServer()->getPluginManager()->disablePlugin($this);
+	 	} 
   }
 	
 
-  public function onCMD(PlayerCommandPreprocessEvent $event):void {
-    $sender = $event->getPlayer();
-    $cmd = $event->getMessage();
-    $prefix = $this->getConfig()->get("prefix");
-    $msg = $this->getConfig()->get("blocked-message");
-  	if(get_class($sender) == "pocketmine\player\Player") {
-		if($cmd[0] == "/") {
-			if (!$sender->hasPermission("levellimiter.bypass")) {
-				if (!$sender->hasPermission("levellimiter.bypass." . $sender->getWorld()->getFolderName())) {
-					$cmdo = trim($cmd, "/");
-					if (!$sender->hasPermission("levellimiter.bypass." . $sender->getWorld()->getFolderName() . "." . explode(" ",$cmdo)[0])) {
-						$con = $this->getConfig()->getAll();
-						if (isset($con[explode(" ",$cmdo)[0]])) {
-							if (!in_array($sender->getWorld()->getFolderName(), $this->getConfig()->get( explode(" ",$cmdo)[0] ))) {
-								$sender->sendMessage($prefix . C::RESET . " " . $msg);
-								$event->cancel();
-								return;
-							}
-						}
-					}
-			  	}
+  public function onCMD(PlayerCommandPreprocessEvent $event) : void 
+  {
+
+    	$sender = $event->getPlayer();
+    	$cmd = $event->getMessage();
+    	$prefix = $this->getConfig()->get("prefix");
+    	$msg = $this->getConfig()->get("blocked-message");
+  		if(get_class($sender) !== "pocketmine\player\Player") return;
+		if($cmd[0] !== "/") return;
+		if($sender->hasPermission("levellimiter.bypass")) return;
+		if($sender->hasPermission("levellimiter.bypass." . $sender->getWorld()->getFolderName())) return;
+		$cmdo = trim($cmd, "/");
+		if($sender->hasPermission("levellimiter.bypass." . $sender->getWorld()->getFolderName() . "." . explode(" ",$cmdo)[0])) return;
+		$con = $this->getConfig()->getAll();
+		if(isset($con[explode(" ",$cmdo)[0]])) {
+			if($this->getConfig()->get('mode') === 'blacklist') goto BlacklistMode;
+			if(!in_array($sender->getWorld()->getFolderName(), $this->getConfig()->get(explode(" ",$cmdo)[0]))) {
+				Cancel:
+					$sender->sendMessage($prefix . C::RESET . " " . $msg);
+					$event->cancel();
+					return;
+				}
 			}
-		}
-	}
-  }
+
+			BlacklistMode:
+
+			if(in_array($sender->getWorld()->getFolderName(), $this->getConfig()->get(explode(" ",$cmdo)[0]))) goto Cancel;			
+			return;
+
+			
+  	}
+
 }
